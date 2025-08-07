258da88df579c01f71270f52d40a55a6ff2c63a6
playgamefootball83 <playgamefootball83@gmail.com>
2025-08-07 17:09:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-API_ID = 25021528 # Replace with your API ID
-API_HASH = "ea73f0e2e8595b85030aa59a11b3ef3b"  # Replace with your API HASH
-BOT_TOKEN = "8431959941:AAFkKRSc2SkTZDbH50ZRh7KJNW-abyIE0bg"  # Replace with your bot token
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # ये जरूरी है .env से वैल्यू लोड करने के लिए
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
