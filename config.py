911566ee3be797c9200c517b6beea48958653776
playgamefootball83 <playgamefootball83@gmail.com>
2025-08-07 16:16:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_ID = 12345678  # Replace with your API ID
 API_HASH = "your_api_hash"  # Replace with your API HASH
-BOT_TOKEN = "your_bot_token"  # Replace with your bot token
+BOT_TOKEN = "8431959941:AAFkKRSc2SkTZDbH50ZRh7KJNW-abyIE0bg"  # Replace with your bot token
