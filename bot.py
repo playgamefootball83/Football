8f53dd1c591a94437bcad1ff240361147786e5e6
playgamefootball83 <playgamefootball83@gmail.com>
2025-08-08 01:43:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,5 @@ bot = Client(
     "bot",
     api_id=25021528,
     api_hash="ea73f0e2e8595b85030aa59a11b3ef3b",
-    bot_token="
-    8431959941:AAFkKRSc2SkTZDbH50ZRh7KJNW-abyIE0bg"
+    bot_token="8431959941:AAFvXjN0akBkjTTCw6OmgJaKeIFNUM3EQDo"
 )
