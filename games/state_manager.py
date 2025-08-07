ec18c32c9143eefa3b2a74bca89d3e69f82d3ee6
playgamefootball83 <playgamefootball83@gmail.com>
2025-08-07 16:34:46 +0530

    Update state_manager.py

--- games/state_manager.py
+++ games/state_manager.py
@@ -1,15 +1,16 @@
+# This dictionary will hold all active games
 active_games = {}
 
 def create_game(chat_id, game_data):
-    games[chat_id] = game_data
+    active_games[chat_id] = game_data
 
 def get_game(chat_id):
-    return games.get(chat_id)
+    return active_games.get(chat_id)
 
 def update_game(chat_id, new_data):
-    if chat_id in games:
-        games[chat_id].update(new_data)
+    if chat_id in active_games:
+        active_games[chat_id].update(new_data)
 
 def delete_game(chat_id):
-    if chat_id in games:
-        del games[chat_id]
+    if chat_id in active_games:
+        del active_games[chat_id]
