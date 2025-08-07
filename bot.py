322b5ac1d406a3e0679d95de9e9b850ca15b2895
playgamefootball83 <playgamefootball83@gmail.com>
2025-08-07 17:01:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from pyrogram import Client
 bot = Client(
     "bot",
     api_id=25021528,
-    api_hash="ea73f0e2e8595b85030aa59a11b3ef3b",
-    bot_token="8431959941:AAFkKRSc2SkTZDbH50ZRh7KJNW-abyIE0bg"
+    api_hash=ea73f0e2e8595b85030aa59a11b3ef3b,
+    bot_token="
+    8431959941:AAFkKRSc2SkTZDbH50ZRh7KJNW-abyIE0bg
 )
